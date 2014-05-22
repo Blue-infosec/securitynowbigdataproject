c4a6c80c41797030b357799049be05828308623f
Levi DeHaan <levi@levidehaan.com>
2014-05-22 14:48:48 -0600

    updated the python script for ntlk sentiment analysis

--- docker-scripts/spark-0.9.1/spark-base/files/twapp.py
+++ docker-scripts/spark-0.9.1/spark-base/files/twapp.py
@@ -1,4 +1,4 @@
-import sys
+import sys, re
 import nltk
 import pymongo
 import csv
@@ -11,8 +11,10 @@ from nltk.corpus import movie_reviews
 from nltk.tokenize import *
 from nltk import *
 
+
 def word_feats(words):
     return dict([(word, True) for word in words])
+
 
 negids = movie_reviews.fileids('neg')
 posids = movie_reviews.fileids('pos')
@@ -20,8 +22,8 @@ posids = movie_reviews.fileids('pos')
 negfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'neg') for f in negids]
 posfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'pos') for f in posids]
 
-negcutoff = len(negfeats)*3/4
-poscutoff = len(posfeats)*3/4
+negcutoff = len(negfeats) * 3 / 4
+poscutoff = len(posfeats) * 3 / 4
 
 trainfeats = negfeats[:negcutoff] + posfeats[:poscutoff]
 testfeats = negfeats[negcutoff:] + posfeats[poscutoff:]
@@ -32,39 +34,42 @@ print 'accuracy:', nltk.classify.util.accuracy(classifier, testfeats)
 classifier.show_most_informative_features()
 
 mongo = MongoClient('172.17.0.5', 27017)
-mdb = mongo['twapp']
+mdb = mongo['twapp_storage']
+secnow = mongo.securitynow
+secnow_c = secnow.securitynow
 
-def mapper(line):
 
+def mapper(line, title, secfile, idsec):
     post = mdb.posts
-
     tokens = word_tokenize(line)
-
     tagged = pos_tag(tokens)
     ntities = chunk.ne_chunk(tagged)
 
-    sentclas = dict([(word, True) for word in line])
+    newline = line.encode('utf-8')
+
+    sentclas = dict([(word, True) for word in newline])
 
     print "nltk entities:"
     for tag in ntities:
         print "Entity: {0} </entity>".format(tag) if "NNP" in tag else ""
 
-    print "CLASSIFICATION FOR THIS SENTENCE: {0}: {1} /END CLASSIFICATION \n".format(line, classifier.classify(sentclas))
-
-    posting = { "original" : line, "tokens" : tokens, "entities" : ntities, "sentiment" : classifier.classify(sentclas) }
+    posting = {"securitynow_id": idsec, "episode": secfile[3:6], "speaker": title, "original": line, "tokens": tokens,
+               "entities": ntities, "sentiment": classifier.classify(sentclas)}
     post_id = post.insert(posting)
 
     print "saved to mongo, post id: {0}".format(post_id)
 
 
 if __name__ == "__main__":
-
     sc = SparkContext(sys.argv[1], "sentiment")
     lines = sc.textFile(sys.argv[2])
 
-    for linex in lines.take(lines.count()):
-        print linex
-        mapper(linex)
+    for post in secnow_c.find({"Titles": {"$regex": u"LEO"}}):
+        print post
+        mapper(post['Data'], post['Titles'], post['File'], post['_id'])
 
+    for post in secnow_c.find({"Titles": {"$regex": u"STEVE"}}):
+        print post
+        mapper(post['Data'])
 
     print "mapper completed"
