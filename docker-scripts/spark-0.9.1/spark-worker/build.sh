d332acec7a33ec9cedb4cd93ff6ec9654f5c7a00
Levi DeHaan <levi@levidehaan.com>
2014-12-29 12:39:27 -0700

    updating some files, just a backup moving machines.

--- docker-scripts/spark-0.9.1/spark-worker/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -f files/files.hash
-docker rmi spark-worker:0.9.1
-for i in `find . -type f | sed s/"\.\/"//`; do git hash-object $i | tr -d '\n'; echo -e "\t$i"; done > /tmp/files.hash
-mv /tmp/files.hash files/files.hash
-sudo docker build -t spark-worker:0.9.1 .
